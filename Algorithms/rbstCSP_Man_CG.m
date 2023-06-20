104141c1772cc85ab385f24946deea19215f7181
gnodking7 <91911643+gnodking7@users.noreply.github.com>
2023-06-19 17:52:15 -0700

    Add files via upload

---
+++ Algorithms/rbstCSP_Man_CG.m
@@ -0,0 +1,36 @@
+% ---------------------------------------------------- %
+%   rbstCSP_Man_CG: Manopt on Sphere manifold
+%           Conjugate Gradient
+% ---------------------------------------------------- %
+%
+% Utilizes Manopt Toolbox: https://www.manopt.org
+
+function [x, Q, iter] = rbstCSP_Man_CG(SM_MAT, SP_MAT, x0, tol)
+    % Input:
+    %   SM_MAT - function handle
+    %            \Sigma(x) = SM_MAT(x)
+    %            [\Sigma(x) \tilde{\Sigma}(x)] = SM_MAT(x)
+    %   SP_MAT - function handle
+    %            \Sigma(x) = SP_MAT(x)
+    %            [\Sigma(x) \tilde{\Sigma}(x)] = SP_MAT(x)
+    %   x0 - initial vector
+    %   tol - tolerance
+    % Output:
+    %   x - final vector
+    %   Q - array of objective values at each iterate
+    %   iter - number of iterations
+
+    n = length(x0);
+    q_fun = @(x) (x' * SM_MAT(x) * x) / (x' * (SM_MAT(x) + SP_MAT(x)) * x);
+    manifold = stiefelfactory(n, 1);    % sphere manifold
+    problem.M = manifold;
+    problem.cost = @(x) q_fun(x);
+    problem.egrad = @(x) (2 / (x' * (SM_MAT(x) + SP_MAT(x)) * x)) * (SM_MAT(x) * x - q_fun(x) * (SM_MAT(x) + SP_MAT(x)) * x);
+    options.verbosity = 0;
+    options.tolgradnorm = tol;
+    [x, ~, info, ~] = conjugategradient(problem, x0, options);
+    Q = vertcat(info.cost);
+    iter = vertcat(info.iter);
+    iter = iter(end);
+
+end
